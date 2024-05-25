e0310112d008407ee8bbd5ce84212639cb26c0e6
Gust De Wit <gust.dewit@student.kuleuven.be>
2024-05-26 00:56:51 +0200

    Reorganized

--- cass-homework/EXS/S2/Ex5.c
+++
@@ -1,8 +0,0 @@
- #include <stdio.h>
- #include <string.h>
- 
- void main(void) {
-	 char string[100];
-	 printf("Please enter a string you want to know the length of: \n");
-	 fgets(string[], 100, stdin);
- }
--- cass-homework/simple_bitmaps.h
+++
@@ -1,4 +0,0 @@
-// Image is represented as a pointer to a blob of 32 bytes, an array of 16 shorts 
-void print_binary_bitmap(short* im);
-void or_image_with(short* im1, short* im2);
-void invert_image(short* im);
